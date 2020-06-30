58f6a597a7255c2c21ffe7f11335906e26f38c5b
niewall <petersbruder@outlook.de>
2020-06-30 12:43:43 +0200

    no message

--- PythonServerOnPy/testingServos.py
+++ PythonServerOnPy/testingServos.py
@@ -1,6 +1,5 @@
 import RPi.GPIO as GPIO
-from time import sleep
-
+import time
 
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(03, GPIO.OUT)
@@ -11,15 +10,18 @@ def setAngle(angle):
     duty = angle / 18 + 2
     GPIO.output(03, True)
     pwm.ChangeDutyCycle(duty)
-    sleep(1)
+    time.sleep(1)
     GPIO.output(03, False)
     pwm.ChangeDutyCycle(0)
 
 SetAngle(0)
+print("0")
 time.sleep(5)
 SetAngle(90)
+print("90")
 time.sleep(5)
 SetAngle(180)
+print("180")
 time.sleep(5)
 pwm.stop()
 GPIO.cleanup()
