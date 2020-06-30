e6618b7be87d03bb004ac8e6a72a44620a5cf471
niewall <petersbruder@outlook.de>
2020-06-30 13:06:30 +0200

    no message

--- PythonServerOnPy/testingServos.py
+++ PythonServerOnPy/testingServos.py
@@ -8,11 +8,11 @@ GPIO.setup(servoPIN, GPIO.OUT)
 p = GPIO.PWM(servoPIN, 50) # GPIO 17 als PWM mit 50Hz
 p.start(2.5) # Initialisierung
 try:
-  p.ChangeDutyCycle(0)
+  p.ChangeDutyCycle(0.5)
   time.sleep(5)
   p.ChangeDutyCycle(6.25)
   time.sleep(5)
-  p.ChangeDutyCycle(9.375)
+  p.ChangeDutyCycle(6.5)
   time.sleep(5)
   p.ChangeDutyCycle(12.5)
   time.sleep(5)
