878f4ba64dd23f807beaaabf2e68cbd9bf3ec4b0
niewall <petersbruder@outlook.de>
2021-03-24 17:24:45 +0100

    upd

--- PythonServerOnPy/network.py
+++ PythonServerOnPy/network.py
@@ -1,6 +1,7 @@
 import subprocess
 import write
 import keyboard
+import os
 
 ssid = ""
 passkey = ""
@@ -78,10 +79,12 @@ keyboard.on_press(key_press)
 
 def changeNetwork():
 
-    p1 = subprocess.Popen(["wpa_passphrase", ssid, passkey], stdout=subprocess.PIPE)
-    p2 = subprocess.Popen(["sudo", "tee", "-a", "/etc/wpa_supplicant/wpa_supplicant.conf", ">", "/dev/null"], stdin=p1.stdout, stdout=subprocess.PIPE)
-    p1.stdout.close()  # Give p1 a SIGPIPE if p2 dies.
-    output,err = p2.communicate()
+    if len(ssid) > 1 and  len(passkey) > 1:
+        p1 = subprocess.Popen(["wpa_passphrase", ssid, passkey], stdout=subprocess.PIPE)
+        p2 = subprocess.Popen(["sudo", "tee", "-a", "/etc/wpa_supplicant/wpa_supplicant.conf", ">", "/dev/null"], stdin=p1.stdout, stdout=subprocess.PIPE)
+        p1.stdout.close()  # Give p1 a SIGPIPE if p2 dies.
+        output,err = p2.communicate()
+        os.system('sudo reboot')
 
 def networkMenu():
     global ssid
